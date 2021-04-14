42acdc4c9821c0d6bf1161eb24eefc7935498ece
John Moen <entity.john@gmail.com>
2021-04-14 12:37:42 -0500

    refactor bgio boilaerplate

--- src/bgio-contexts/useBgioCtx.tsx
+++ src/bgio-contexts/useBgioCtx.tsx
@@ -1,54 +1,38 @@
-import * as React from "react"
-import { BoardProps } from "boardgame.io/react"
+import * as React from "react";
+import { BoardProps } from "boardgame.io/react";
 
-import { useBgioClientInfo } from "./useBgioClientInfo"
-import { phaseNames, stageNames } from "game/constants"
+import { useBgioClientInfo } from "./useBgioClientInfo";
 
 type BgioCtxProviderProps = {
-  children: React.ReactNode
-  ctx: BoardProps["ctx"]
-}
+  children: React.ReactNode;
+  ctx: BoardProps["ctx"];
+};
 
 // add two handy properties
 type BgioCtxValue = {
-  ctx: BoardProps["ctx"] & {
-    isMyTurn: boolean
-    isSetupPhase: boolean
-    isGameover: boolean
-  }
-}
-const BgioCtxContext = React.createContext<BgioCtxValue | undefined>(undefined)
+  ctx: BoardProps["ctx"] & {};
+};
+const BgioCtxContext = React.createContext<BgioCtxValue | undefined>(undefined);
 
 export function BgioCtxProvider({ ctx, children }: BgioCtxProviderProps) {
-  const { playerID } = useBgioClientInfo()
-  const isMyTurn: boolean = ctx.currentPlayer === playerID
-  const isSetupPhase: boolean = ctx.phase === phaseNames.setup
-  const isOrderMarkerPhase: boolean = ctx.phase === phaseNames.placeOrderMarkers
-  const isPlacementPhase: boolean = ctx.phase === phaseNames.placement
-  const isRoundOfPlayPhase: boolean = ctx.phase === phaseNames.roundOfPlay
-  const isAttackingStage: boolean =
-    isRoundOfPlayPhase && ctx.activePlayers?.[playerID] === stageNames.attacking
-  const isGameover: boolean = Boolean(ctx.gameover)
+  const { playerID } = useBgioClientInfo();
   return (
     <BgioCtxContext.Provider
       value={{
         ctx: {
           ...ctx,
-          isMyTurn,
-          isGameover,
-          isSetupPhase,
         },
       }}
     >
       {children}
     </BgioCtxContext.Provider>
-  )
+  );
 }
 
 export function useBgioCtx() {
-  const context = React.useContext(BgioCtxContext)
+  const context = React.useContext(BgioCtxContext);
   if (context === undefined) {
-    throw new Error("useBgioCtx must be used within a BgioCtxProvider")
+    throw new Error("useBgioCtx must be used within a BgioCtxProvider");
   }
-  return context
+  return context;
 }
